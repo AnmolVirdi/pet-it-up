8744e15fe91721587e3526746036229198d6323c
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2021-02-14 17:06:30 +0530

    mail confirmation and we tried our best

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ def home():
     if 'email' in session:
         email=session['email']
         return render_template('index.html')
-    return render_template('index.html')
+    return render_template('signup.html')
 
 
 @app.route('/signup',methods=['POST','GET'])
 def signup():
     if request.method=='POST':
         form_details=request.form
-        if form_details['pass']==from_details['re-pass']:
+        if form_details['pass']==from_details['re_pass']:
             email=form_details['email']
             # print(email,"hehehehehheheheh")
             token = s.dumps(email,salt='email-confirm')
@@ -42,14 +42,16 @@ def signup():
 def login():
     if request.method=='POST':
         form_details = request.form
-        data=api.login(form_details['email'],form_details['pass'])
-        if(data=='correct_password'):
-            session['email']=form_details['email']
-            return render_template('index.html')
-        elif data=='wrong_password':
-            return render_template('login.html')
-        elif data == 'username_dosenot_exist':
-            return  render_template('login.html')
+        if api.mail_confirm(form_details['email'])==True:
+            data=api.login(form_details['email'],form_details['pass'])
+            if(data=='correct_password'):
+                session['email']=form_details['email']
+                return render_template('index.html')
+            elif data=='wrong_password':
+                return render_template('login.html')
+            elif data == 'username_dosenot_exist':
+                return  render_template('login.html')
+        return  render_template('login.html')   
     return render_template('login.html')
 
 
@@ -57,6 +59,17 @@ def login():
 def logout():
     session.pop('email',None)
     return render_template('login.html')
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email=s.loads(token,salt='email-confirm',max_age=600)
+        api.update(email)
+        return render_template('untitled.html')
+    except SignatureExpired:
+        email=s.loads(token,salt='email-confirm')
+        api.delete(email)
+        return '<h1>Token expired</h1>'   
 
 
 if __name__=='__main__':
--- db.py
+++ db.py
@@ -41,3 +41,57 @@ def login(email,password):
             return 'username_dosenot_exist'    
     except Exception as e:
         print(e)
+
+
+
+def update(email):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql="update usermaster set email_confirmation=(%s) where email=(%s)"
+            temp=2
+            curr.execute(sql,(temp,email))
+            conn.commit()
+    except Exception as e:
+        print(e)     
+
+def mail_confirm(email):     
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )   
+    try:
+        with conn.cursor() as curr:
+            sql="select email_confirmation from usermaster where email=(%s)"
+            curr.execute(sql,(email))
+            output=curr.fetchone()
+            if(output[0]=="2"): return True
+            return False
+            # conn.commit()
+    except Exception as e:
+        print(e)     
+
+def delete(email):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )   
+    try:
+        with conn.cursor() as curr:
+            sql="delete from usermaster where email=(%s)"
+            curr.execute(sql,(email))
+            conn.commit()
+    except Exception as e:
+        print(e)
